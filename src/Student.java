6e65b91b16c768ba804b4c4fcfa1bb1122cfa834
Priya Kumaresan <pkumares@gaul.csd.uwo.ca>
2017-02-14 13:22:13 -0500

    Committing changes from slide 43

--- src/Student.java
+++ src/Student.java
@@ -3,7 +3,7 @@
  */
 public class Student extends Person {
     String major;
-
+    List<Course> courseList;
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
@@ -11,5 +11,9 @@ public class Student extends Person {
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course c){
+        courseList.add(c);
     }
 }
